e23db5037ae6c2e31c5d4911c9bc9cd3dc6b377b
NathanielERowe <nathanielrowe2001@gmail.com>
2024-03-29 15:42:42 -0500

    Clean up and cfg

--- dune_tension/gCodeDriver.py
+++ dune_tension/gCodeDriver.py
@@ -1,36 +1,103 @@
 import re
+import json
 import platform
 import time
 import pandas as pd
+import os.path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
+def make_config_comp(calx, caly, calwire, delx, dely, minwirenum, maxwirenum):
+    currdict = {}
+
+    for wire in range(minwirenum, maxwirenum+1):
+        currdict[wire] = {}
+        currdict[wire]["X"] = calx+(wire-calwire)*delx
+        currdict[wire]["Y"] = caly+(wire-calwire)*dely
+    
+    return currdict
+
+def make_config(APAstr):
+    layer = None
+    apa_dict = {}
+
+    layer = input("Enter layer or quit (X, V, U, G, q): ")
+    while(layer!="q"):
+        apa_dict[layer]={}
+
+        calbool = 'y'
+        calx = []
+        caly = []
+        calwires = []
+        print("Enter new calibration point(s): ")
+   
+        if(layer == "U" or layer == "V"):
+           Ncalpts = 2
+        else:
+           Ncalpts = 1
+ 
+        for n in range(Ncalpts):
+            x = float(input("Enter calibration point X: "))
+            y = float(input("Enter calibration point Y: "))
+            calwire = int(input("Enter wire number of the calibration point: "))
+            calx.append(x)
+            caly.append(y)
+            calwires.append(calwire)
+
+        if layer == "X":
+            # delx, dely, minwirenum, maxwirenum
+            layer_dict = make_config_comp(calx[0], caly[0], calwires[0], 0, -4.79166667, 1, 480)
+        elif layer == "V":
+            # Zone 1
+            Vz1_p1 = make_config_comp(calx[0], caly[0], calwires[0], 2.72455392, -3.79161799, 8, 218)
+            Vz1_p2 = make_config_comp(Vz1_p1[218]["X"], Vz1_p1[218]["Y"], 218, 0.0, -5.75, 219, 399)
+            Vz1 = Vz1_p1 | Vz1_p2
+
+            # Zone 2
+            Vz2 = make_config_comp(2800, (Vz1[399]["Y"]-5.75)+(2800-Vz1[399]["X"])*5.75/8, 
+                                   400, 0.0, -5.75, 400, 551)
+
+            # Zone 4
+            Vz4 = make_config_comp(5150, Vz2[551]["Y"]+(5150-Vz2[400]["X"])*5.75/8, 
+                                   400, 0.0, -5.75, 552, 751)
+
+            # Zone 5
+            Vz5_p1 = make_config_comp(calx[1], caly[1], calwires[1], 2.72455392, -3.79161799, 992, 1146)
+            Vz5_p2 = make_config_comp(Vz5_p1[992]["X"], Vz5_p1[992]["Y"], 992, 0.0, -5.75, 752, 991)
+            Vz5 = Vz5_p1 | Vz5_p2
+
+            layer_dict = Vz1 | Vz2 | Vz4 | Vz5
+        apa_dict[layer] = layer_dict
+        layer = input("Enter layer or quit (X, V, U, G, q): ")
+
+    out_file = open(APAstr+"_cfg.json", "w") 
+    json.dump(apa_dict, out_file, indent = 6) 
+    out_file.close() 
+
 def zone(x):
     if(x < 2400.0):
         return 1
-    elif(2400.0 < x and x < 5000):
+    elif(2400.0 < x and x < 5000.0):
         return 2
-    elif(5000 < x and x < 6500):
+    elif(5000.0 < x and x < 6500.0):
         return 4
-    elif(6500 < x and x < 7000):
+    elif(6500.0 < x and x < 7000.0):
         return 5
-        
-
-def make_hdf():
-    xlsx = ExcelFile('TensionGcodeOye.xlsx')
-    for sheetind in range(len(xlsx.sheet_names)):
-        df = xlsx.parse(xlsx.sheet_names[sheetind])
-        sheetstr = xlsx.sheet_names[sheetind]
-        df.to_hdf(sheetstr+".df", key="Misc") 
 
 def find_wire_pos(wirenum, layer):
+    # hard coded for now, will fix
+    f = open('Wood_cfg.json')
+ 
+    # returns JSON object as 
+    # a dictionary
+    wire_dict = json.load(f)
+
     # must be fixed, only works for Z1 vlayer now
-    wiredf = pd.read_hdf("V_FULL_B_Z1.df")
-    x = wiredf[wiredf.Wire==wirenum].X.to_numpy()[0]
-    y = wiredf[wiredf.Wire==wirenum].Y.to_numpy()[0]
+    x = wire_dict[layer][str(wirenum)]["X"]
+    y = wire_dict[layer][str(wirenum)]["Y"]
     return x, y
 
 def find_wire_gcode(wirenum, layer):
@@ -95,7 +162,7 @@ class laser:
         cmd = find_wire_gcode(des_wire, "V")
         if(self.layer == "U" or self.layer == "V"):
             ini_zone = zone(self.pos_x)
-            des_zone = zone(find_wire_pos(des_wire)[0])
+            des_zone = zone(find_wire_pos(des_wire, self.layer)[0])
             if(ini_zone == des_zone):
                 manual_g_code(cmd)
             else:
@@ -204,15 +271,20 @@ def manual_g_code(cmd):
 
 
 if __name__ == "__main__":
-    manual_g_code("X2112 Y987.8")
-    test = laser("V", 400)
-    print(test.pos_x)
-    print(test.pos_y)
-    print(test.wirenum)
-    print(test.layer)
-
-    test.move_to_wire(399)
-    print(test.pos_x)
-    print(test.pos_y)
-    print(test.wirenum)
-    print(test.layer)
+    if not os.path.isfile("Wood_cfg.json"):
+        make_config("Wood")
+    print(find_wire_pos(300, "V"))
+#    manual_g_code("X2112 Y987.8")
+#    test = laser("V", 400)
+#    print(test.pos_x)
+#    print(test.pos_y)
+#    print(test.wirenum)
+#    print(test.layer)
+#    print(zone(test.pos_x))
+#
+#    test.move_to_wire(399)
+#    print(test.pos_x)
+#    print(test.pos_y)
+#    print(test.wirenum)
+#    print(test.layer)
+#    print(zone(test.pos_x))
--- dune_tension/g_code_test.py
+++
@@ -1,4 +0,0 @@
-from gCodeDriver import click_step_button
-
-if __name__ == "__main__":
-    click_step_button()
