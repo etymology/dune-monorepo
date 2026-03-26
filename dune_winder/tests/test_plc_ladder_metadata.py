7d9ad995d5284472c235e828fc71f66626826a6f
DUNE_admin <benjamin.oye@gmail.com>
2026-03-26 11:02:26 -0500

    add configs and calibrations from winder

--- dune_winder/tests/test_plc_ladder_metadata.py
+++ dune_winder/tests/test_plc_ladder_metadata.py
@@ -16,6 +16,22 @@ class PlcLadderMetadataTests(unittest.TestCase):
     self.assertIn("MoveXY_State_2_3", metadata.programs)
     self.assertIn("MOTION_INSTRUCTION", metadata.udts)
 
+  def test_tag_store_seeds_controller_and_program_values(self):
+    metadata = load_plc_metadata(PLC_ROOT)
+    tags = TagStore(metadata)
+
+    self.assertEqual(tags.get("STATE"), 0)
+    self.assertEqual(tags.get("QueueCtl.POS", program="motionQueue"), 0)
+    self.assertEqual(tags.get("CurSeg.Valid", program="motionQueue"), False)
+
+    tags.set("QueueCtl.POS", 7, program="motionQueue")
+    tags.set("CurSeg.Valid", True, program="motionQueue")
+    tags.set("Z_AXIS_STAT[5].PC", True, program="MoveZ_State_4_5")
+
+    self.assertEqual(tags.get("QueueCtl.POS", program="motionQueue"), 7)
+    self.assertEqual(tags.get("CurSeg.Valid", program="motionQueue"), True)
+    self.assertEqual(tags.get("Z_AXIS_STAT[5].PC", program="MoveZ_State_4_5"), True)
+
   def test_program_tags_shadow_controller_tags(self):
     metadata = load_plc_metadata(PLC_ROOT)
     tags = TagStore(metadata)
